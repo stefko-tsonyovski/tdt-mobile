dd3679e7600741f550568c4ac4db23d9ee53ef3e
stefko-tsonyovski <116901971+stefko-tsonyovski@users.noreply.github.com>
2023-03-15 11:19:55 +0200

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://192.168.120.122:5000/api/v1";
+// export const BASE_URL = "http://192.168.120.122:5000/api/v1";
 // export const BASE_URL = "http://192.168.0.18:5000/api/v1";
-// export const BASE_URL = "https://tdt-server-github.vercel.app/api/v1";
+export const BASE_URL = "https://tdt-server-github.vercel.app/api/v1";
 
 export const PLAYERS_INITIAL_PAGE = 1;
 export const PLAYERS_ITEMS_PER_PAGE = 8;
