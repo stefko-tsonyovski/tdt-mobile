d099cbff9c9aa57bf7369ebd4b37586088a0eb71
Miroslav Uzunov <miroslav.uzunov@scalefocus.com>
2023-03-23 16:02:24 +0200

    added push notification when user join from invitation

--- src/features/players/screens/player-details/player-details.screen.tsx
+++ src/features/players/screens/player-details/player-details.screen.tsx
@@ -1,18 +1,10 @@
 import React, { FC } from "react";
-import { FlatList, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { FlatList } from "react-native";
 
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { Text } from "../../../../components/typography/text.component";
 import { RankListRootStackParamList } from "../../../../infrastructure/navigation/rank-list.navigator";
 import { useSinglePlayer } from "../../../../services/players/players.service";
-import CountryFlag from "react-native-country-flag";
-import {
-  CardHeaderPlayerContainer,
-  PlayerImage,
-} from "../../components/player-details/player-details.styles";
 import { Spacer } from "../../../../components/spacer/spacer.component";
-import { Card, Colors, Divider } from "react-native-paper";
 import { PlayerResults } from "../../components/player-results/player-results.component";
 import Spinner from "react-native-loading-spinner-overlay/lib";
 import { colors } from "../../../../infrastructure/theme/colors";
